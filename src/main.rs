3f4cd1719587e9e9be39f624bd24c057db12189b
Brian Li <brianchunghli@gmail.com>
2025-10-24 15:49:55 +1100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use axum::{Router, routing::get};
+use random_word::Lang;
+
+#[derive(serde::Serialize)]
+struct RandomWord {
+    word: &'static str,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route(
+        "/",
+        get(|| async {
+            axum::extract::Json(RandomWord {
+                word: random_word::get(Lang::En),
+            })
+        }),
+    );
+    let port = 49152;
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{port}"))
+        .await
+        .unwrap_or_else(|_| panic!("unable to bind to port {port}; time to die!"));
+    println!("serving at http://localhost:{port}");
+    axum::serve(listener, app)
+        .await
+        .expect("failed to serve; time to die!");
+}
